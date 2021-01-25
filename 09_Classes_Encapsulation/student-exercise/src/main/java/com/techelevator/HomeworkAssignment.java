1d087153438c9662d08f445298253d73dcdbec35
jane Akusoba <jane.akusoba@gmail.com>
2021-01-24 23:26:41 -0500

    Attempted exercises

--- 09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ 09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,60 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+	private int earnedMarks;
+	private int possibleMarks;
+	private String submitterName;
+	
+	
+	public HomeworkAssignment(int possibleMarks, String submitterName) {
+		this.possibleMarks = possibleMarks;
+		this.submitterName = submitterName;
+	}
+	
+	public String getLetterGrade () {
+		double grade = ((double) earnedMarks / (double) possibleMarks) * 100;
+
+		if(grade >= 90) {
+			return "A";
+		}
+		else {
+			if(grade < 90 && grade >=80) {
+				return "B";
+			}
+			else {
+				if(grade < 80 && grade >= 70) {
+					return "C";
+				}
+				else {
+					if(grade < 70 && grade >= 60) {
+						return "D";
+					}
+					else {
+						return "F";
+					}
+				}
+			}
+		}
+		
+	}
+	
+	
+	public int getearnedMarks() {
+		return earnedMarks;
+	}
+
+
+	public void setearnedMarks(int earnedMarks) {
+		this.earnedMarks = earnedMarks;
+	}
+
+
+	public String getSubmitterName() {
+		return submitterName;
+	}
+
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
 
 }
