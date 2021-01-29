3e6100e19fe3387291f2c4eba7f7a7cc51b31504
jane Akusoba <jane.akusoba@gmail.com>
2021-01-29 10:43:07 -0500

    m1 assessment

--- A1_Module_1_Week3_Assessment/src/main/java/com/techelevator/FlowerShopApp.java
+++ A1_Module_1_Week3_Assessment/src/main/java/com/techelevator/FlowerShopApp.java
@@ -4,6 +4,9 @@ public class FlowerShopApp {
 
 	public static void main(String[] args) {
 		FlowerShopOrder fso = new FlowerShopOrder("Standard", 2 );
+		double dt = fso.deliveryTotal(true, "20000");
+
+		System.out.println(dt);
 
 	}
 
