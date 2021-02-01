31e1686da5d770e4821c4b4d18baf8c97bcde95c
Steve Carmichael <steve@techelevator.com>
2021-02-01 14:16:54 -0500

    added final lecture code

---
+++ 17_FileIO_Reading_in/instructor-lecture/src/main/java/com/techelevator/exceptions/Student.java
@@ -0,0 +1,40 @@
+package com.techelevator.exceptions;
+
+public class Student {
+	
+	private int id;
+	private String first;
+	private String section;
+	private String animal;
+
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getSection() {
+		return section;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+	public String getAnimal() {
+		return animal;
+	}
+	public void setAnimal(String animal) {
+		this.animal = animal;
+	}
+
+
+
+
+
+}
