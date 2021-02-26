6449b87767b6ed791335faaa0660765f078be58e
jane Akusoba <jane.akusoba@gmail.com>
2021-02-26 08:22:49 -0500

    Started HW

--- module-2/11_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/11_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -1,11 +1,19 @@
 package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.DAO.AuctionDAO;
+
 import com.techelevator.auctions.DAO.MemoryAuctionDAO;
 import com.techelevator.auctions.model.Auction;
+import com.techelevator.reservations.models.Hotel;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 
 import java.util.List;
+
+
 
 @RestController
 @RequestMapping("/auctions")
@@ -16,5 +24,42 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDAO();
     }
-
+    
+    @RequestMapping(path = "/auctions", method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam(required = false, defaultValue = "") String title_like, @RequestParam(required = false, defaultValue = 0) double currentBid_lte) {
+    	List<Auction> auctionList  = null;
+    
+    	if (title_like != null) {
+	    	
+		    auctionList = dao.list();
+    	}
+    	else if(currentBid_lte != 0) {
+    		auctionList = dao.searchByPrice(currentBid_lte);
+    	}
+    	else if(title_like != null && currentBid_lte != 0 ) {
+    		 auctionList = dao.searchByTitleAndPrice(title_like, currentBid_lte);
+    		
+    	}
+    	else {
+    		auctionList = dao.searchByTitle(title_like);
+    	}
+    	return auctionList;
+    	
+    }
+    
+    @RequestMapping(path = "/auctions/{id}", method = RequestMethod.GET)
+    public Auction get (@PathVariable int id) {
+    	return dao.get(id);
+    }
+    
+    
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/auctions", method = RequestMethod.POST)
+    public Auction create (@RequestBody Auction auction) {
+           
+        return dao.create(auction);
+    }
+    
+   
+    
 }
