455906e58dd01fd65dc50b087fc950fa0ddc8cb3
Steve Carmichael <steve@techelevator.com>
2021-03-26 13:19:28 -0400

    added review solution from class

---
+++ module-3/M3_Week3_Vue_Review/vue-front-end-solution/src/components/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+   <div>
+       <img src="../assets/java-green.png" alt="Our logo">
+       <h1>{{ pageTitle }}</h1>
+       <nav>
+           <a href="#">Home</a>
+           <a href="#">About</a>
+           <a href="#">Home Search</a>
+       </nav>
+   </div>
+
+</template>
+
+<script>
+export default {
+    name: 'the-header',
+    data() {
+      return {
+          pageTitle: 'Java Green Home Search'
+      };
+
+    }
+}
+</script>
+
+<style scoped>
+
+   img {
+       width: 15%;
+       height: auto;
+   }
+
+   nav {
+       display: flex;
+       justify-content: center;
+   }
+
+   nav a {
+       padding: 15px 25px;
+       background-color: rgb(37, 241, 37);
+   }
+
+   nav a:hover {
+       background-color: green;
+       color: white;
+   }
+
+</style>
